fc2f55caf53a6391dafd788e7cffa08edc4726d9
Erik Gassler <erikgassler@live.com>
2023-08-04 09:50:34 -0500

    fix youtube display

--- webapp/src/pages/home.rs
+++ webapp/src/pages/home.rs
@@ -3,13 +3,15 @@ use crate::prelude::*;
 /// App home page
 pub(crate) fn page_home(_contexts: Contexts) -> Html {
     set_title("Professional Software Engineer & Website Developer");
-    let video = r#"<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/IVPHtC0H2fU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>"#;
+    let video = r#"<iframe width="3840" height="2160" src="https://www.youtube-nocookie.com/embed/IVPHtC0H2fU" title="YouTube video player" style="width:calc(0.8 * var(--window-width));height:calc(0.5625 * 0.8 * var(--window-width));" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>"#;
     html! {
         <>
             <MarkdownContent href="/d/en-US/home/intro.md" />
             <CaseBooks link_to_blog={true} />
             <MarkdownContent href="/d/en-US/home.md" />
-            <HtmlContent html={video} />
+            <Paper class="ma-2">
+                <HtmlContent html={video} />
+            </Paper>
             <MarkdownContent href="/d/en-US/cards/interest_cards.md" />
             <NextPageButton url="/software-engineering-standards-and-practices" />
         </>
