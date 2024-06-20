a9dcd9aa4dab090e4c6c88fb973a64bf3da4509b
Erik Gassler <erik@gassler.us>
2024-06-20 14:06:59 -0500

    fix check for p tag

--- webapp/wc/static-vs-agile-comp-table.js
+++ webapp/wc/static-vs-agile-comp-table.js
@@ -5,6 +5,13 @@
         constructor() {
             super();
             const t = this;
+            if (t.parentNode && t.parentNode.nodeName === 'P') {
+                let p = t.parentNode;
+                t.parentNode.parentNode.insertBefore(t, t.parentNode);
+                if (p.innerHTML.trim() === '') {
+                    p.remove();
+                }
+            }
         }
         static get observedAttributes() {
             return [];
